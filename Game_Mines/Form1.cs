fc1afefbb5c309f04b69b4a42d889ddfb91b32e1
Александр <lecon@list.ru>
2022-02-19 09:34:17 +0300

    Сделали карту

--- Game_Mines/Form1.cs
+++ Game_Mines/Form1.cs
@@ -12,9 +12,27 @@ namespace Game_Mines
 {
     public partial class Form1 : Form
     {
+        int _mapWisthCount = 10;
+        int _mapHeightCount = 10;
+        int _bombs = 20;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var panel = new Panel();
+            panel. Dock = DockStyle.Fill;
+            for (int i = 0; i < _mapWisthCount; i++)
+            {
+                for (int j = 0; j < _mapHeightCount; j++)
+                {
+                    var btn = new Button() { Width=20, Height=20, Left=i*20, Top=j*20};
+                    panel.Controls.Add(btn);
+                }
+            }
+            this.Controls.Add(panel);
         }
     }
 }
